1b487281f3c10bda89f7cab8520642eac9459bc7
tink2123 <y_tink@163.com>
2018-10-29 16:17:49 +0800

    add script file

---
+++ doc/fluid/api/gen_index.py
@@ -0,0 +1,21 @@
+import os.path, time
+import exceptions
+import glob
+import os
+if __name__ == '__main__':
+    
+    file_object = open('reorder.rst', 'w')
+    file_object.write('''=============
+API Reference
+=============
+
+..  toctree::
+    :maxdepth: 1
+
+''')
+    file_object.write('    api_guides/index.rst'+'\n')
+    file_object.write('    fluid.rst'+'\n')
+    for file_name in sorted(glob.glob("*.rst")):
+        if file_name != ('index_en.rst' and 'fluid.rst'):
+            file_object.write('    '+file_name + "\n")
+    file_object.close( )
