f01477b132837fa0c581aac76787dcbf75c44f36
Zeng Jinle <32832641+sneaxiy@users.noreply.github.com>
2019-06-12 13:54:48 +0800

    split layers,test=develop (#889)

--- doc/fluid/api/gen_doc.sh
+++ doc/fluid/api/gen_doc.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-python gen_doc.py layers --submodules control_flow device io nn ops tensor learning_rate_scheduler detection metric_op > layers.rst
+
+mkdir -p layers
+
+for module in control_flow io nn ops tensor learning_rate_scheduler detection metric_op
+do
+  python gen_doc.py --module layers.${module} --module_prefix layers > layers/${module}.rst
+done 
 
 for module in data_feeder dataset clip metrics executor initializer io nets optimizer profiler regularizer transpiler recordio_writer backward average profiler unique_name
 do
-  python gen_doc.py ${module} > ${module}.rst
+  python gen_doc.py --module ${module} --module_prefix ${module} > ${module}.rst
 done
 
-python gen_doc.py "" > fluid.rst
+python gen_doc.py --module "" --module_prefix "" > fluid.rst
+
+python gen_index.py
