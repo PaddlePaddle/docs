f3dc7f7c85df68794be45ed7607efc233432d54f
tianshuo78520a <707759223@qq.com>
2019-07-22 15:08:32 +0800

    add check_approval

--- scripts/checkapproval.sh
+++ scripts/checkapproval.sh
@@ -9,8 +9,11 @@ for API_FILE in ${API_FILES[*]}; do
       APPROVALS=`echo ${approval_line}|python ./scripts/check_pr_approval.py 2 7534971 14105589 12605721 3064195 328693 47554610 39645414 11195205 20274488 45024560 ` 
     fi
   fi
+  echo ${APPROVALS}
   if [ "${APPROVALS}" == "FALSE" ]; then
+    echo 111
     if [ "${API_FILE}" == "doc/fluid" ];then
+      echo 222
       echo "You must have two RD (wanghaoshuang or guoshengCS or heavengate or kuke or Superjomn or lanxianghit or cyj1986 or hutuxian or frankwhzhang or nepeplwu) approval for the api change! ${API_FILE} for the management reason of API interface and API document."
     fi
     exit 1
