9a0e9235fe73aee130970a57faeed5b487a8eaf8
tianshuo78520a <707759223@qq.com>
2020-01-19 19:08:47 +0800

    test=fluid

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,5 +2,5 @@
 
 PATH="/home/FluidDoc"
 
-bash -x ${PATH}/scripts/checkapproval.sh
-bash -x ${PATH}/scripts/check_api_cn.sh
+/bin/bash -x ${PATH}/scripts/checkapproval.sh
+/bin/bash -x ${PATH}/scripts/check_api_cn.sh
