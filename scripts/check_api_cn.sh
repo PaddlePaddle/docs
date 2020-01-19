c9e7049f0357e5e5effa7632bd56d08c85661001
tianshuo78520a <707759223@qq.com>
2020-01-19 16:15:53 +0800

    test=fluid_test

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -6,11 +6,11 @@ for files in git_files;do
     echo $files|grep 'FluidDoc/doc/fluid/api_cn/.*/.*.rst'
     if [ $? -eq 0 ];then
         if [ "$night" == "develop" ];then
-           pip install paddlepaddle-gpu
+           pip install paddlepaddle
         else
            git clone https://github.com/PaddlePaddle/Paddle.git
            mkdir build && cd build
-           cmake .. -DWITH_GPU=ON -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
+           cmake .. -DWITH_GPU=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
            pip install Paddle/build/opt/paddle/share/wheels/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
            paddle version
         fi
