8c95079a7e78d97f4155c943a5771f3a7296f03e
tianshuo78520a <707759223@qq.com>
2020-01-20 15:08:14 +0800

    test

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -3,7 +3,6 @@
 git_files=`git diff --numstat upstream/$BRANCH`
 
 for files in `echo $git_files`;do
-    echo $files,123
     echo $files|grep 'doc/fluid/api_cn/.*/.*.rst'
     if [ $? -eq 0 ];then
         if [ "$night" == "develop" ];then
@@ -11,7 +10,7 @@ for files in `echo $git_files`;do
            pip install paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl
         else
            git clone https://github.com/PaddlePaddle/Paddle.git
-           mkdir build && cd build
+           mkdir Paddle/build && cd Paddle/build
            cmake .. -DWITH_GPU=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
            pip install Paddle/build/opt/paddle/share/wheels/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
         fi
