e392bd544797d803bf7e8c4c23b6eb1993bad376
tianshuo78520a <707759223@qq.com>
2020-01-20 18:40:26 +0800

    test

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -13,6 +13,7 @@ for files in `echo $git_files`;do
            mkdir Paddle/build && cd Paddle/build
            cmake .. -DWITH_GPU=OFF -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
            make -j`nproc`
+           find . -name "*.whl"
            pip install python/dist/paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl 
         fi
         paddle version
