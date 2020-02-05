4498ea37b82081f714384b43bb51cc20f79078fc
tianshuo78520a <707759223@qq.com>
2020-02-05 14:33:26 +0800

    test

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -6,12 +6,12 @@ for files in `echo $git_files`;do
     echo $files|grep 'doc/fluid/api_cn/.*/.*.rst'
     if [ $? -eq 0 ];then
         if [ "$night" == "develop" ];then
-           wget -q https://paddle-wheel.bj.bcebos.com/0.0.0-cpu-mkl/paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl
+           wget -q https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda9-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl 
            pip install -U paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl
         else
            git clone https://github.com/PaddlePaddle/Paddle.git
            mkdir Paddle/build && cd Paddle/build
-           cmake .. -DWITH_GPU=OFF -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
+           cmake .. -DWITH_GPU=ON -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
            make -j`nproc`
            find . -name "*.whl"
            pip install -U python/dist/paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl 
