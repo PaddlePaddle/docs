da69a809baf259941af07ba18caad3edbea1cb6e
tianshuo78520a <707759223@qq.com>
2020-02-05 17:44:04 +0800

    test

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -12,7 +12,7 @@ else
    cmake .. -DWITH_GPU=ON -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
    make -j`nproc`
    find . -name "*.whl"
-   pip install -U python/dist/paddlepaddle-0.0.0-cp27-cp27mu-linux_x86_64.whl
+   pip install -U paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl 
 fi
 
 for files in `echo $git_files`;do
