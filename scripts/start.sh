efda3a97780c95ce2fec59a14ac541fe7ff81456
tianshuo78520a <707759223@qq.com>
2020-02-20 15:10:53 +0800

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,8 @@
 
 DIR_PATH="/FluidDoc"
 
-/bin/bash ${DIR_PATH}/scripts/checkapproval.sh
+/bin/bash ${DIR_PATH}/scripts/check_api_cn.sh
 if [ $? -ne 0 ];then
   exit 1
 fi
-/bin/bash ${DIR_PATH}/scripts/check_api_cn.sh
+/bin/bash ${DIR_PATH}/scripts/checkapproval.sh
