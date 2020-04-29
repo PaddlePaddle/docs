33918a903716ebdc66afbea0f6e6a662f4a547c6
tianshuo78520a <707759223@qq.com>
2020-04-29 14:46:46 +0800

    add nn.* api

--- doc/fluid/api/gen_doc.sh
+++ doc/fluid/api/gen_doc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# fluid
 for module in layers dataset clip metrics executor initializer io nets optimizer profiler regularizer transpiler backward profiler unique_name dygraph
 do
   python gen_doc.py --module_name ${module} --module_prefix ${module} --output ${module} --output_name fluid --to_multiple_files True
@@ -9,6 +10,7 @@ done
 python gen_doc.py --module_name "" --module_prefix "" --output fluid --output_name fluid --to_multiple_files True
 python gen_module_index.py fluid  fluid
 
+# tensor
 for module in math random stat
 do
   python gen_doc.py --module_name ${module} --module_prefix ${module} --output ${module} --output_name tensor --to_multiple_files True --output_dir tensor
@@ -16,14 +18,6 @@ do
 done
 
 python gen_module_index.py tensor paddle.tensor
-
-for module in loss
-do
-  python gen_doc.py --module_name ${module} --module_prefix ${module} --output ${module} --output_name nn --to_multiple_files True --output_dir nn
-  python gen_module_index.py nn.${module} ${module}
-done
-
-python gen_module_index.py nn paddle.nn
 
 for module in math manipulation
 do
@@ -34,5 +28,17 @@ done
 python gen_module_index.py complex.tensor tensor
 python gen_module_index.py complex paddle.complex
 
+# nn
+for module in loss
+do
+  python gen_doc.py --module_name ${module} --module_prefix ${module} --output ${module} --output_name nn --to_multiple_files True --output_dir nn
+  python gen_module_index.py nn.${module} ${module}
+done
+
+python gen_doc.py --module_name "" --module_prefix "" --output nn --output_name nn --to_multiple_files True
+python gen_module_index.py nn paddle.nn
+
+
+# index.rst
 python gen_index.py
 
