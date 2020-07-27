64e58eb32400f3bd1cd1a2d6fb272a2c9a747799
tianshuo78520a <707759223@qq.com>
2020-07-27 15:22:40 +0800

    Add cache (#2304)
    * add cache
    
    * fix dir
    
    * change
    
    * change
    
    * change
    
    * change
    
    * fix dir

--- scripts/check_api_cn.sh
+++ scripts/check_api_cn.sh
@@ -7,9 +7,8 @@ if [ "$night" == "develop" ];then
    wget -q https://paddle-wheel.bj.bcebos.com/0.0.0-gpu-cuda9-cudnn7-mkl/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
    pip install -U paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl
 else
-   git clone https://github.com/PaddlePaddle/Paddle.git
-   mkdir Paddle/build && cd Paddle/build
-   cmake .. -DWITH_GPU=ON -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
+   cd Paddle/build
+   cmake .. -DWITH_GPU=ON  -DWITH_COVERAGE=OFF -DWITH_TESTING=OFF -DCMAKE_BUILD_TYPE=Release
    make -j`nproc`
    pip install -U python/dist/paddlepaddle_gpu-0.0.0-cp27-cp27mu-linux_x86_64.whl 
 fi
