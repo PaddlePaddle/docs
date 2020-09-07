4c7ac8af2a95809f3586c29058cecbe3dc24d1cd
chentianyu03 <chentianyu03@baidu.com>
2020-09-07 15:30:13 +0000

    fix path error

--- ci_scripts/check_api_cn.sh
+++ ci_scripts/check_api_cn.sh
@@ -3,14 +3,14 @@
 git_files=`git diff --numstat upstream/$BRANCH | awk '{print $NF}'`
 
 for file in `echo $git_files`;do
-  grep "code-block" $files
+  grep "code-block" ../$file
   if [ $? -eq 0 ] ;then 
     echo $file | grep "doc/paddle/api/paddle/.*_cn.rst"
     if [ $? -eq 0 ];then
         api_file=`echo $file | sed 's#doc/paddle/api/##g'`
         grep -w "${api_file}" ${DIR_PATH}/api_white_list.txt
         if [ $? -ne 0 ];then
-            python chinese_samplecode_processor.py $file
+            python chinese_samplecode_processor.py ../$file
             if [ $? -ne 0 ];then
                 echo "chinese sample code failed"
                 exit 5
