6b4014f2e46c65351e6d2aa35f1cef66afd8ddd6
Ren Wei <wadefelix@gmail.com>
2021-04-14 15:06:55 +0800

    convert paddle.fluid.core_avx.* to paddle.fluid.core.*
    大多数core_avx下的api都改成core了，但是CPUPlace竟然还是逃脱了，换个方案试一试，在这个地方加

--- doc/paddle/api/gen_doc.py
+++ doc/paddle/api/gen_doc.py
@@ -90,7 +90,6 @@ def get_all_api(root_path='paddle', attr="__all__"):
 # step 1 fill field : `id` & `all_names`, type, docstring
 def process_module(m, attr="__all__"):
     api_counter = 0
-    pat = re.compile(r'paddle\.fluid\.core_[\w\d]+\.(.*)$')
     if hasattr(m, attr):
         # may have duplication of api
         for api in set(getattr(m, attr)):
@@ -126,11 +125,6 @@ def process_module(m, attr="__all__"):
                     if docstr:
                         api_info_dict[fc_id]["docstring"] = inspect.cleandoc(
                             docstr)
-                    # paddle.fluid.core_avx.* -> paddle.fluid.core.*
-                    mo = pat.match(full_name)
-                    if mo:
-                        api_info_dict[fc_id]["all_names"].add(
-                            'paddle.fluid.core.' + mo.group(1))
     return api_counter
 
 
@@ -686,8 +680,15 @@ class EnDocGenerator(object):
 
 
 def filter_api_info_dict():
+    pat = re.compile(r'paddle\.fluid\.core_[\w\d]+\.(.*)$')
     for id_api in api_info_dict:
         if "all_names" in api_info_dict[id_api]:
+            if "full_name" in api_info_dict[id_api]:
+                # paddle.fluid.core_avx.* -> paddle.fluid.core.*
+                mo = pat.match(api_info_dict[id_api]["full_name"])
+                if mo:
+                    api_info_dict[id_api]["all_names"].add('paddle.fluid.core.'
+                                                           + mo.group(1))
             api_info_dict[id_api]["all_names"] = list(
                 api_info_dict[id_api]["all_names"])
         if "object" in api_info_dict[id_api]:
