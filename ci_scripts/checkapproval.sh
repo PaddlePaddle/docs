b97c58e35192bf520870dc5e6b55fdebf63a4cc5
Ren Wei <wadefelix@gmail.com>
2021-05-17 18:02:56 +0800

    project name changed from FluidDoc to docs

--- ci_scripts/checkapproval.sh
+++ ci_scripts/checkapproval.sh
@@ -7,7 +7,7 @@ API_FILES=("docs/api/paddle")
 for API_FILE in ${API_FILES[*]}; do
   API_CHANGE=`git diff --name-only upstream/$BRANCH | grep "${API_FILE}"`
   if [ "${API_CHANGE}" ];then
-    approval_line=`curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/FluidDoc/pulls/${GIT_PR_ID}/reviews?per_page=10000`
+    approval_line=`curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/${GIT_PR_ID}/reviews?per_page=10000`
     if [ "${API_FILE}" == "docs/api/paddle" ];then
       APPROVALS=`echo ${approval_line} | python ./check_pr_approval.py 1 2870059 27208573 29231 28379894 23093488 11935832`
     fi
