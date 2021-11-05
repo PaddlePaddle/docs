7ac0302d7d52ee96c2425c5167e16fa5850410d8
Ren Wei <wadefelix@gmail.com>
2021-11-05 16:01:37 +0800

    skip the mod when its __file__ is None

--- docs/api/gen_doc.py
+++ docs/api/gen_doc.py
@@ -229,6 +229,7 @@ def parse_module_file(mod):
         if (isinstance(src_file, str) and isinstance(src_file, str)):
             logger.error('%s: mod_name=%s, src_file=%s',
                          str(mod), mod_name, src_file)
+            return
         logger.debug("parsing %s:%s", mod_name, src_file)
         if len(mod_name) >= 6 and mod_name[:6] == 'paddle':
             if os.path.splitext(src_file)[1].lower() == '.py':
