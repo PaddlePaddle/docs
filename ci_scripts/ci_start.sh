2257f867f085cdef49e7124850405dacdb0699d7
Ren Wei <wadefelix@gmail.com>
2021-12-30 19:42:33 +0800

    install jq
    s

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -9,12 +9,15 @@ elif [ ${BRANCH} = 'release/2.1' ] ; then
 fi
 export PADDLE_WHL
 
+cat /etc/issue
+apt update && apt install -yq --no-install-recommends jq
+
 JOB_URL_PREFIX="http://10.24.2.236/job/doc-review-only-for-1.8/buildWithParameters?token=auto&SourcePaddlePR=&CLEAN_PADDLE_BUILD_FIRST=false&DEPLOY_AFTER_BUILD=false"
 #PaddleWhlAddr=${PADDLE_WHL//:/%3A}
 #PaddleWhlAddr=${PaddleWhlAddr//\//%2F}
 PaddleWhlAddr=$(echo ${PADDLE_WHL} | sed -e 's/:/%3A/' -e 's@/@%2F@g')
 BRANCH_SHORT=$(echo ${BRANCH} | sed 's@release/@@')
-GITHUB_LOGIN=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/4173 | jq '.user.login')
+GITHUB_LOGIN=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/${AGILE_PULL_ID} | jq '.user.login')
 OA_ID=$(curl -H "Host: sz-cpu-agent01.bcc-szth.baidu.com" "http://10.24.2.236:8091/v1/user/id_convert?to=oa&id=${GITHUB_LOGIN}" | sed 's/"//g')
 curl -H "Host: sz-cpu-agent01.bcc-szth.baidu.com" \
   "${JOB_URL_PREFIX}&SourceDocsPRBR=${AGILE_PULL_ID}&PaddleWhlAddr=${PaddleWhlAddr}&PADDLE_VERSIONSTR=${BRANCH_SHORT}&EMAIL=${OA_ID}"
