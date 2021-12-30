247af21996b2e2016d7ae9e4c734e778981caeea
Ren Wei <wadefelix@gmail.com>
2021-12-30 20:48:41 +0800

    再测试一把，为何被调用了两次

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -9,12 +9,8 @@ elif [ ${BRANCH} = 'release/2.1' ] ; then
 fi
 export PADDLE_WHL
 
-# cat /etc/issue # ubuntu 18.04
-# apt update && apt install -yq --no-install-recommends jq
 
 JOB_URL_PREFIX="http://10.24.2.236/job/doc-review-only-for-1.8/buildWithParameters?token=auto&SourcePaddlePR=&CLEAN_PADDLE_BUILD_FIRST=false&DEPLOY_AFTER_BUILD=false"
-#PaddleWhlAddr=${PADDLE_WHL//:/%3A}
-#PaddleWhlAddr=${PaddleWhlAddr//\//%2F}
 PaddleWhlAddr=$(echo ${PADDLE_WHL} | sed -e 's/:/%3A/' -e 's@/@%2F@g')
 BRANCH_SHORT=$(echo ${BRANCH} | sed 's@release/@@')
 # GITHUB_LOGIN=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/${AGILE_PULL_ID} | jq '.user.login')
