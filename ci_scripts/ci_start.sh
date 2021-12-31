b72e0ce7ddede67076933ac547c413217b8c4ffc
Ren Wei <wadefelix@gmail.com>
2021-12-31 11:21:26 +0800

    configurations by Environment Variables

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -11,14 +11,17 @@ export PADDLE_WHL
 
 DOCS_BUILD_AUTO=${DOCS_BUILD_AUTO:-true}
 if [ "${DOCS_BUILD_AUTO}" = "true" ] ; then
-    JOB_URL_PREFIX="http://10.24.2.236/job/doc-review-only-for-1.8/buildWithParameters?token=auto&SourcePaddlePR=&CLEAN_PADDLE_BUILD_FIRST=false&DEPLOY_AFTER_BUILD=false"
+    DOCS_BUILD_TOKEN=${DOCS_BUILD_TOKEN:-auto}
+    DOCS_BUILD_JOB=${DOCS_BUILD_JOB:-"http://10.24.2.236/job/doc-review-only-for-1.8/buildWithParameters"}
+    DOCS_BUILD_JOB_HOST=${DOCS_BUILD_JOB_HOST:-"sz-cpu-agent01.bcc-szth.baidu.com"}
+    JOB_URL_PREFIX="${DOCS_BUILD_JOB}?token=${DOCS_BUILD_TOKEN}&SourcePaddlePR=&CLEAN_PADDLE_BUILD_FIRST=false&DEPLOY_AFTER_BUILD=false"
     PaddleWhlAddr=$(echo ${PADDLE_WHL} | sed -e 's/:/%3A/' -e 's@/@%2F@g')
     BRANCH_SHORT=$(echo ${BRANCH} | sed 's@release/@@')
     # GITHUB_LOGIN=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/${AGILE_PULL_ID} | jq '.user.login')
     GITHUB_LOGIN=$(curl -H "Authorization: token ${GITHUB_API_TOKEN}" https://api.github.com/repos/PaddlePaddle/docs/pulls/${AGILE_PULL_ID} | grep '^    "login"' | sed 's/^.*"login": "\(.*\)".*/\1/g')
     OA_ID=$(curl -H "Host: sz-cpu-agent01.bcc-szth.baidu.com" "http://10.24.2.236:8091/v1/user/id_convert?to=oa&id=${GITHUB_LOGIN}" | sed 's/"//g')
     echo "Hello ${GITHUB_LOGIN} ${OA_ID}, docs-build run automatically."
-    curl -H "Host: sz-cpu-agent01.bcc-szth.baidu.com" \
+    curl -H "Host: ${DOCS_BUILD_JOB_HOST}" \
         "${JOB_URL_PREFIX}&SourceDocsPRBR=${AGILE_PULL_ID}&PaddleWhlAddr=${PaddleWhlAddr}&PADDLE_VERSIONSTR=${BRANCH_SHORT}&EMAIL=${OA_ID}"
 fi
 
