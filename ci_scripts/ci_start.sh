04795e8c99e3d739520090506f813173f08fe9bd
Ren Wei <wadefelix@gmail.com>
2022-02-09 17:37:43 +0800

    define PADDLE_WHL in ci_scripts.sh

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -2,6 +2,14 @@
 
 export DIR_PATH=${PWD}
 
+PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl
+if [ ${BRANCH} = 'release/2.2' ] ; then
+    PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-2.2.0-cp38-cp38-linux_x86_64.whl
+elif [ ${BRANCH} = 'release/2.1' ] ; then
+    PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-2.1.0-cp38-cp38-linux_x86_64.whl
+fi
+export PADDLE_WHL
+
 /bin/bash  ${DIR_PATH}/check_code.sh
 if [ $? -ne 0 ];then
     echo "code format error"
