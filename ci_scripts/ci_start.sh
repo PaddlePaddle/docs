f462876f35036d390c612d1bee8979534b559e34
Ren Wei <wadefelix@gmail.com>
2022-02-10 14:54:26 +0800

    Sync files to bos

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -22,9 +22,27 @@ if [ $? -ne 0 ];then
 fi
 
 if [ "${BUILD_DOC}" = "true" ] &&  [ -x /usr/local/bin/sphinx-build ] ; then
+    export OUTPUTDIR=/docs
+    export VERSIONSTR=$(echo ${BRANCH} | sed 's@release/@@g')
     /bin/bash -x ${DIR_PATH}/gendoc.sh
     if [ $? -ne 0 ];then
         exit 1
+    fi
+    if [ -n "${BOS_CREDENTIAL_AK}" ] && [ -n "${BOS_CREDENTIAL_SK}" ] ; then
+        echo "Ak = ${BOS_CREDENTIAL_AK}" >> ${BOSCMD_CONFIG}/credentials
+        echo "Sk = ${BOS_CREDENTIAL_SK}" >> ${BOSCMD_CONFIG}/credentials
+    fi
+    # [系统参数如下](https://cloud.baidu.com/doc/XLY/s/qjwvy89pc#%E7%B3%BB%E7%BB%9F%E5%8F%82%E6%95%B0%E5%A6%82%E4%B8%8B)
+    if [ "${UPLOAD_DOC}" = "true" ] ; then
+        PREVIEW_JOB_NAME="preview-${AGILE_PIPELINE_ID}-${AGILE_PIPELINE_BUILD_ID}"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/en/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/en/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/en/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/en/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/zh/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/zh/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/zn/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/zh/${VERSIONSTR}/_sources/"
     fi
 fi
 
