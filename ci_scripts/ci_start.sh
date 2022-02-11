24f17b11679c9299c4a8bbd2fc6654049e648e87
Ren Wei (任卫) <wadefelix@gmail.com>
2022-02-11 14:10:59 +0800

    Add document generating script, and the function is under control of EnvVar `BUILD_DOC` and `UPLOAD_DOC` (#4211)
    * the origin gendoc.sh copied from our doc-sphinx project
    
    * define PADDLE_WHL in ci_scripts.sh
    
    * call gendoc.sh in ci_start.sh
    
    * code style
    
    * check if sphinx installed
    
    * code style
    
    * use BUILD_DOC=true to control the gendoc function
    
    * skip install pytest
    
    * Sync files to bos
    
    * disable trace the credential keys
    
    * typo
    
    * "preview-pr-<pr number>" as the doc preview name
    
    * code style
    
    * no need to reserve the codes for branch 1.x
    
    * as gendoc will modify many files in docs/api/paddle, do checkapproval before it.
    
    * if PADDLE_WHL pre-defined, use it
    
    * emtpy string test

--- ci_scripts/ci_start.sh
+++ ci_scripts/ci_start.sh
@@ -2,6 +2,16 @@
 
 export DIR_PATH=${PWD}
 
+if [ -z "${PADDLE_WHL}" ] ; then
+    PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-0.0.0-cp38-cp38-linux_x86_64.whl
+    if [ ${BRANCH} = 'release/2.2' ] ; then
+        PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-2.2.0-cp38-cp38-linux_x86_64.whl
+    elif [ ${BRANCH} = 'release/2.1' ] ; then
+        PADDLE_WHL=https://paddle-fluiddoc-ci.bj.bcebos.com/python/dist/paddlepaddle_gpu-2.1.0-cp38-cp38-linux_x86_64.whl
+    fi
+fi
+export PADDLE_WHL
+
 /bin/bash  ${DIR_PATH}/check_code.sh
 if [ $? -ne 0 ];then
     echo "code format error"
@@ -10,7 +20,39 @@ fi
 
 /bin/bash -x ${DIR_PATH}/check_api_cn.sh
 if [ $? -ne 0 ];then
-  exit 1
+    exit 1
 fi
 
 /bin/bash  ${DIR_PATH}/checkapproval.sh
+if [ $? -ne 0 ];then
+    exit 1
+fi
+
+if [ "${BUILD_DOC}" = "true" ] &&  [ -x /usr/local/bin/sphinx-build ] ; then
+    export OUTPUTDIR=/docs
+    export VERSIONSTR=$(echo ${BRANCH} | sed 's@release/@@g')
+    /bin/bash -x ${DIR_PATH}/gendoc.sh
+    if [ $? -ne 0 ];then
+        exit 1
+    fi
+    set +x
+    if [ -n "${BOS_CREDENTIAL_AK}" ] && [ -n "${BOS_CREDENTIAL_SK}" ] ; then
+        echo "Ak = ${BOS_CREDENTIAL_AK}" >> ${BCECMD_CONFIG}/credentials
+        echo "Sk = ${BOS_CREDENTIAL_SK}" >> ${BCECMD_CONFIG}/credentials
+    fi
+    set -x
+    # [系统参数如下](https://cloud.baidu.com/doc/XLY/s/qjwvy89pc#%E7%B3%BB%E7%BB%9F%E5%8F%82%E6%95%B0%E5%A6%82%E4%B8%8B)
+    # ${AGILE_PIPELINE_ID}-${AGILE_PIPELINE_BUILD_ID}"
+    if [ "${UPLOAD_DOC}" = "true" ] ; then
+        PREVIEW_JOB_NAME="preview-pr-${GIT_PR_ID}"
+        BOSBUCKET=${BOSBUCKET:=paddle-site-web-dev}
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/en/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/en/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/en/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/en/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/en/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/zh/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/zh/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/zh/${VERSIONSTR}/_sources/"
+        ${BCECMD} --conf-path ${BCECMD_CONFIG} bos sync "${OUTPUTDIR}/zh/${VERSIONSTR}" "bos:/${BOSBUCKET}/documentation/zh/${PREVIEW_JOB_NAME}" \
+            --delete --yes --exclude "${OUTPUTDIR}/zh/${VERSIONSTR}/_sources/"
+    fi
+fi
