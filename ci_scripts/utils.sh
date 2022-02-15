380afaff0873ab09e4fe9188b1210520e36c5765
Ren Wei (任卫) <wadefelix@gmail.com>
2022-02-15 18:44:01 +0800

    use the paddle.whl built by pipline PR-CI-Build as the preferred in api-check and doc-build procedure (#4212)
    * add some utils functions
    
    * try to get PADDLEPADDLE_PR=123 from pr's body, and use it to download the whl from PR-CI-Build pipline
    
    * replace the slash in repo_name
    
    * print PADDLE_WHL
    
    * shell array usage
    
    * fetch pr info with githut token
    
    github token
    
    * install paddle in not installed
    
    * sphinx build thread num limit
    
    * output some debug messages
    
    * checkapproval after build doc, so should clean git repo after build doc and before checkapproval
    
    * pylint allready install in image, pytest is not necessary.
    
    * print preview url
    
    * print preview url at the end.
    
    * http not https

---
+++ ci_scripts/utils.sh
@@ -0,0 +1,71 @@
+#! /bin/bash
+
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+function get_repo_pr_info(){
+    # such as "PaddlePaddle/docs"
+    repo_name=$1
+    # such as 4211
+    pr_num=$2
+    if [ -z "${repo_name}" ] || [ -z "${pr_num}" ] ; then
+        return 1
+    fi
+    tmpfile="/tmp/${repo_name//\//-}-${pr_num}-info.json"
+    set +x
+    curl -sL -o ${tmpfile} -H "Accept: application/vnd.github.v3+json" \
+        -H "Authorization: token ${GITHUB_API_TOKEN}" \
+        https://api.github.com/repos/${repo_name}/pulls/${pr_num}
+    set -x
+    if [ $? -ne 0 ] ; then
+        return 2
+    fi
+    echo ${tmpfile}
+    return 0
+}
+
+function get_latest_commit_from_pr_info(){
+    # get_repo_pr_info's output
+    pr_info_file=$1
+    if [ ! -r ${pr_info_file} ] ; then
+        return 1
+    fi
+    echo $(jq -r '.head.sha' ${pr_info_file})
+    return 0
+}
+
+function get_paddle_pr_num_from_docs_pr_info(){
+    # get_repo_pr_info's output
+    pr_info_file=$1
+    if [ ! -r ${pr_info_file} ] ; then
+        return 1
+    fi
+
+    declare -A arr_kv
+    while read line
+    do
+        echo "$line" | grep '^\w\+\s*=\s*.*' > /dev/null
+        if [ $? = 0 ] ; then
+            kv=($(echo $line | sed 's/=/\n/g'))
+            k=($(echo "${kv[0]}" | sed 's/\s//g'))
+            v=($(echo "${kv[1]}" | sed 's/^\s*//g' | sed 's/\s*$//g'))
+            # arr_kv[${kv[1]}]=${kv[2]}
+            arr_kv[${k}]=${v}
+        fi
+    done < <(jq -r '.body' ${pr_info_file})
+
+    echo ${arr_kv[PADDLEPADDLE_PR]}
+    return 0
+}
