a0143e9c35ddcd03a43642f0b21e1fe415998dbc
Ren Wei <wadefelix@gmail.com>
2022-03-21 17:10:20 +0800

    echo the var

--- ci_scripts/check_api_cn.sh
+++ ci_scripts/check_api_cn.sh
@@ -31,6 +31,7 @@ function filter_cn_api_files() {
 need_check_cn_doc_files=$(find_all_cn_api_files_modified_by_pr)
 echo $need_check_cn_doc_files
 filter_cn_api_files "${need_check_cn_doc_files}" need_check_files
+echo "$need_check_files"
 if [ "$need_check_files" = "" ]
 then
     echo "need check files is empty, skip chinese api check"
