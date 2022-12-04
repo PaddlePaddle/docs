64d0bf20eebb6717f438dc9edb621a52295d1441
Shaun2016 <1549118476@qq.com>
2022-12-04 20:31:59 +0800

    reformat check_copy_from_parsed_into_sample_code.py

--- ci_scripts/check_api_cn.sh
+++ ci_scripts/check_api_cn.sh
@@ -37,6 +37,7 @@ then
     echo "need check files is empty, skip chinese api check"
 else
     # Check COPY-FROM is parsed into Sample Code
+    echo "run COPY-FROM parsed check"
     python check_copy_from_parsed_into_sample_code.py $need_check_files
     if [ $? -ne 0 ];then
         echo "exists COPY-FROM has not been parsed into sample code, please check COPY-FROM in these files"
--- ci_scripts/check_copy_from_parsed_into_sample_code.py
+++ ci_scripts/check_copy_from_parsed_into_sample_code.py
@@ -9,26 +9,31 @@ def check_copy_from_not_parsed(file):
         for line, i in enumerate(f):
             if 'COPY-FROM' in i:
                 error_parsed.append(i)
-                print(file, 'line: ', line, i, ' is not parsed into sample code, \
-                    please check the api name after COPY-FROM')
+                print(
+                    file,
+                    "line: ",
+                    line,
+                    i,
+                    " is not parsed into sample code, \
+                    please check the api name after COPY-FROM",
+                )
     return error_parsed
+
 
 def run(input_files):
     all_error_parsed = []
     files = input_files.split()
     for file in files:
-        error_parsed = check_copy_from_not_parsed('../docs/' + file)
+        error_parsed = check_copy_from_not_parsed("../docs/" + file)
         all_error_parsed.extend(error_parsed)
     if all_error_parsed:
         sys.exit(1)
-    print('All COPY-FROM parsed success in PR !')
+    print("All COPY-FROM parsed success in PR !")
+
 
 if len(sys.argv) < 2:
     print("Error: inadequate number of arguments")
     print("Please input one file path")
     sys.exit(1)
 else:
-    if not os.path.exists(sys.argv[1]):
-        print("File not found")
-        sys.exit(1)
     res = run(sys.argv[1])
