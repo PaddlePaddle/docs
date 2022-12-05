547c92a656a232d6a8d343e4322bb6cf1b6bd297
Shaun2016 <1549118476@qq.com>
2022-12-05 14:53:31 +0800

    add file extension check

--- ci_scripts/check_copy_from_parsed_into_sample_code.py
+++ ci_scripts/check_copy_from_parsed_into_sample_code.py
@@ -5,6 +5,8 @@ import os
 
 def check_copy_from_not_parsed(file):
     error_parsed = []
+    if '_cn.rst' not in file:
+        return error_parsed
     with open(file, 'r') as f:
         for line, i in enumerate(f):
             if 'COPY-FROM' in i:
@@ -20,13 +22,13 @@ def check_copy_from_not_parsed(file):
     return error_parsed
 
 
-def run(input_files):
-    print('COPY-FROM check files: ', input_files)
+def run_copy_from_check(pr_files):
+    print('COPY-FROM check files: ', pr_files)
     all_error_parsed = []
-    if not input_files:
-        print("input_files list is empty, skip COPY-FROM check")
+    if not pr_files:
+        print("pr file list is empty, skip COPY-FROM check")
         sys.exit(0)
-    for file in input_files:
+    for file in pr_files:
         error_parsed = check_copy_from_not_parsed("../docs/" + file)
         all_error_parsed.extend(error_parsed)
     if all_error_parsed:
@@ -39,4 +41,4 @@ if len(sys.argv) < 2:
     print("Please input one file path")
     sys.exit(1)
 else:
-    res = run(sys.argv[1:])
+    res = run_copy_from_check(sys.argv[1:])
