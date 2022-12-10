a063f8728821ea62e22b16b181269883326c6432
Shaun2016 <1549118476@qq.com>
2022-12-11 05:49:11 +0800

    fix check_api_docs_en.sh parameters

--- ci_scripts/check_api_docs_en.sh
+++ ci_scripts/check_api_docs_en.sh
@@ -14,8 +14,8 @@ function check_system_message(){
 }
 
 echo "RUN Engish API Docs Checks"
-need_check_api_py_files="$1"
-jsonfn="$2"
-output_path="$3"
+jsonfn=$1
+output_path=$2
+need_check_api_py_files=${3}
 echo "RUN System Message MARNING/ERROR Check"
-check_system_message $need_check_api_py_files $jsonfn $output_path
+check_system_message $jsonfn $output_path $need_check_api_py_files
