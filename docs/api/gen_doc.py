f3da6671993a488e50e4c71f862ebbb7c58d7d65
ooooo <3164076421@qq.com>
2023-10-24 10:53:52 +0800

    CodeStyle for PLR1701

--- docs/api/gen_doc.py
+++ docs/api/gen_doc.py
@@ -266,10 +266,7 @@ def parse_module_file(mod):
                 for node in mod_ast.body:
                     short_names = []
                     if (
-                        (
-                            isinstance(node, ast.ClassDef)
-                            or isinstance(node, ast.FunctionDef)
-                        )
+                        (isinstance(node, (ast.ClassDef, ast.FunctionDef)))
                         and hasattr(node, 'name')
                         and hasattr(sys.modules[mod_name], node.name)
                         and node.name[0] != '_'
