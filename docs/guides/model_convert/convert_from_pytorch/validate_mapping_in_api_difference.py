52ab3e60d2747189007028bfe9be538bada176ed
RedContritio <RedContritio@qq.com>
2024-04-08 13:09:03 +0800

    update table headers in `pytorch_api_mapping_cn.md` (#6589)
    * update table headers
    
    * update doc header

--- docs/guides/model_convert/convert_from_pytorch/validate_mapping_in_api_difference.py
+++ docs/guides/model_convert/convert_from_pytorch/validate_mapping_in_api_difference.py
@@ -399,10 +399,10 @@ def process_mapping_index(index_path, item_processer, context={}):
 
     expect_column_names = [
         "序号",
-        "PyTorch-2.1",
-        "PaddlePaddle-dev",
+        "Pytorch 最新 release",
+        "Paddle develop",
         "映射关系分类",
-        "详细对比",
+        "备注",
     ]
 
     context["table_row_idx"] = context.get("table_row_idx", -1)
