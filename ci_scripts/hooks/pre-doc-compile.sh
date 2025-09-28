bf3de9ff9554aecdcb331118de84bfdfd80c6675
hd9568 <2358440725@qq.com>
2025-09-28 04:12:58 +0000

    调试CI执行结果

--- ci_scripts/hooks/pre-doc-compile.sh
+++ ci_scripts/hooks/pre-doc-compile.sh
@@ -52,6 +52,9 @@ fi
 python ${APIMAPPING_ROOT}/tools/get_api_difference_info.py
 python ${APIMAPPING_ROOT}/tools/generate_pytorch_api_mapping.py
 
+ls ${APIMAPPING_ROOT}/tools
+head -n 100 ${APIMAPPING_ROOT}/pytorch_api_mapping_cn.md
+
 if [ $? -ne 0 ]; then
     echo "Error: API mapping generate script failed, please check changes in ${APIMAPPING_ROOT}"
     exit 1
