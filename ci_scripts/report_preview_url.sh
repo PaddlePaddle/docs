bc5d8c5600367643c73a2338a2f9d6fac51a610a
ooooo <3164076421@qq.com>
2025-10-08 21:56:44 +0800

    fix typos

--- ci_scripts/report_preview_url.sh
+++ ci_scripts/report_preview_url.sh
@@ -31,7 +31,7 @@ done
 if [ ${#output_lines[@]} -gt 0 ]; then
     cat <<-EOF
 > [!CAUTION]
-> 如果你不清楚这个 pr 的修改内容（特别是修改了 \`report_preview_url.sh\` 或 \`preview-url-comment.yml\`），请不要随便点击链接！！！
+> 如果你不清楚这个 pr 的修改内容（特别是修改了 \`report_preview_url.sh\` 或 \`preview-url-generate.yml\`），请不要随便点击链接！！！
 
 <details>
 <summary>📚 本次 PR 文档预览链接 (点击展开)</summary>
