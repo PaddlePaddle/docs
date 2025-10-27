9ca407fec1576052bbfc53d6cddee0c69b3b77a3
algorithm1832 <737634857@qq.com>
2025-10-27 16:47:31 +0800

    add None default value processing

--- docs/api/gen_doc.py
+++ docs/api/gen_doc.py
@@ -412,7 +412,9 @@ def gen_function_args_string(fn_node: ast.FunctionDef) -> str:
         arg_list.append("*")
     for i, kwonlyarg in enumerate(fn_node.args.kwonlyargs):
         kwdefault = fn_node.args.kw_defaults[i]
-        if kwdefault is not None:
+        if kwdefault is None:
+            arg_list.append(kwonlyarg.arg)
+        else:
             default_value_str = _gen_default_value_string(kwdefault)
             arg_list.append(f"{kwonlyarg.arg}={default_value_str}")
     # kwarg
